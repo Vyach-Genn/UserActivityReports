6a37996d206640573dd24ca8ce5e1deaf87c2b74
vachg <vachgen@yandex.ru>
2025-04-27 11:30:10 +0300

    Start

---
+++ src/main/java/prosky/user_activity_reports/UserActivityReportsApplication.java
@@ -0,0 +1,13 @@
+package prosky.user_activity_reports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserActivityReportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserActivityReportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prosky/user_activity_reports/UserActivityReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package prosky.user_activity_reports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserActivityReportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
